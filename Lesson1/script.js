9cc77ef81392e3186d05c9c2f11a5a7d1ec1f4a8
Bohdan Tchernii <bodiachernii@gmail.com>
2022-07-22 12:09:45 +0300

    lesson1

---
+++ Lesson1/script.js
@@ -0,0 +1,27 @@
+const hello = 'hello'
+const owu ='owu'
+const com ='com'
+const num1 = 1
+const ua ='ua'
+const num10 = 10
+const num999 = -999
+const num123 = 123
+const pi = 3.14
+const num2=2.7
+const num16 = 16
+const bool1 = true
+const bool0 = false
+document.write(hello,owu,com,ua,num1,num10,num999,num123,pi,num2,num16,bool1,bool0);
+const name = 'Bohdan';
+const srName = 'Tchernii';
+const surName = 'Viktorovych';
+const person = name + ' ' + srName + ' ' + surName
+document.write(`<br> ${person}`)
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a,typeof b, typeof c)
+const getName = window.prompt('Whats your name')
+const getSrName = window.prompt('Whats your ser name')
+const getSurName = window.prompt ('Whats your sur name')
+console.log(getName+' '+getSrName+' '+getSurName)
