6c23bbe7f46786ad88a0f76cdf60a906cd12f31b
promius <bodiachernii@gmail.com>
2022-07-27 14:02:19 +0300

    lesson2

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,54 +1,62 @@
 const array = ['Vasyl', 'Andriy', 'Bohdan', 'Roman', 'Uriy', 12, 31, 21, 11, 5]
-for (let i = 0; i <= array.length; i++) {
-  console.log(array[i])
-}
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
+console.log(array[5])
+console.log(array[6])
+console.log(array[7])
+console.log(array[8])
+console.log(array[9])
+
 const gameOfThrones = {
   title: 'a song of ice and fire',
   genre: 'epic fantasy',
   pageCount: 1500,
+  authors: [
+    {
+      name: 'George Martin',
+      age: 73,
+    },
+    {
+      name: 'Phyllis Eisenstein',
+      age: 74,
+    }
+  ]
 }
 const bible = {
   title: 'Holy Bible',
   genre: 'documentary',
   pageCount: 2000,
+  authors: [
+    {
+      name: 'Solomon',
+      age: 33,
+    },
+    {
+      name: 'David',
+      age: 60,
+    }
+  ]
 }
 const jsForChildren = {
   title: 'JavaScript For Children',
   genre: 'educational',
   pageCount: 368,
+  authors: [
+    {
+      name: 'Nick Morgan',
+      age: 69,
+    }
+  ]
 }
 
-gameOfThrones.authors = [
-  {
-    name: 'George Martin',
-    age: 73,
-  },
-  {
-    name: 'Phyllis Eisenstein',
-    age: 74,
-  }
-]
-bible.authors = [
-  {
-    name: 'Solomon',
-    age: 33,
-  },
-  {
-    name: 'David',
-    age: 60,
-  }
-]
-jsForChildren.authors = [
-  {
-    name: 'Nick Morgan',
-    age: 69,
-  }
-]
 const users = [
   {
     name: 'Bohdan',
     userName: 'Bohdan',
-    password:12345678,
+    password: 12345678,
     age: 21,
   },
   {
@@ -107,9 +115,13 @@ const users = [
   }
 ]
 
-
-
-  users.forEach((item)=>{
-  console.log(item.password)
-})
-// console.log(users[0].password)
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)
